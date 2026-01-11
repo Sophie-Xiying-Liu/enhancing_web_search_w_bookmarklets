204ebc65d2bb664853d6ca9dbff1c18647589319
Sophie-Xiying-Liu <xiyliu@gmail.com>
2026-01-11 12:33:22 +0100

    chore: set up project structure and configuration

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+];
